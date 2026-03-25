940de3bdba047503ab2eed3b492500962d50ce84
hiro <hiroto117@Hirotovivobook.localdomain>
2026-03-25 11:56:51 +0900

    new files

--- python/learning.py
+++ python/learning.py
@@ -2,4 +2,13 @@ import numpy as np
 import librosa
 import librosa.display
 from pathlib import Path
-from sklearn.cross_decomposition
+from sklearn import datasets
+
+data_dir = Path('testdata')
+npy_files = list(data_dir.glob('**/*.npy'))
+
+X = []
+Y = []
+
+for npy_files in npy_files:
+    mfccs = np.load(npy_files)
