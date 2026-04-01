aaaed9ebe15d0e0f58609f7c1b62b0313231fdef
hiro <hiroto117@Hirotovivobook.localdomain>
2026-04-01 23:22:35 +0900

    prefin app

--- python/app.py
+++ python/app.py
@@ -12,23 +12,34 @@ def predict(text):
     # 2次元配列を変更する処理
     mean = np.mean(mfccs, axis=1)
     std = np.std(mfccs, axis=1)
-    concatenate = np.concatenate([mean, std])
+    concatenate = np.conancatete([mean, std])
     
-    
+    # 音楽ジャンル
+    class_genru = {
+        "blues", "classical", "country", "disco", "environment", "hiphop", "jazz", "metal", "pop", "reggae", "rock"
+    }
     # 学習モデルの反映
     scaler = joblib.load('expscaler.pkl')
     model = joblib.load('expmodel.pkl')
+    
+    transform = scaler.transform()
+    logits = model.predict()
+    
+    genru = {}
+    for i, class_genrus in enumerate(class_genru):
+        genru[class_genru] = float(probs[0][i])
+        
 
     return genru
 
 
 # Grインターフェイスの作成
-demo = gr.Interface{
+demo = gr.Interface(
     fn=predict,
-    inputs=gr.Audio(type='filepath')
-    outputs=
-    title="音楽ジャンル測定アプリ"
-}
+    inputs=gr.Audio(type='filepath'),
+    outputs=gr.Label(num_top_classes=11, label='予測ジャンル'),
+    title="音楽ジャンル測定アプリ",
+)
 
 # アプリケーションの気道
 if __name__ == "__main__":
